68747edd3b92bc279927a09cf9e9092ac0242fa1
aimeechengdev <aimeechengdev@gmail.com>
2015-05-31 00:18:18 -0700

    add post

--- app/src/main/java/com/example/bc/camera/MainActivity.java
+++ app/src/main/java/com/example/bc/camera/MainActivity.java
@@ -1,25 +1,43 @@
 package com.example.bc.camera;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.io.File;
 
 
 public class MainActivity extends ActionBarActivity {
     TextView flowerName;
     private File imageFile;
+    Uri imgUri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,12 +68,34 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    private void doPostRequest(){
 
+        String urlString = "http://rasnacis.lv/vova.php";
+        try
+        {
+            HttpClient client = new DefaultHttpClient();
+            //HttpClient httpClient = HttpClientBuilder.create().build();
+            HttpPost post = new HttpPost(urlString);
+
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+            nameValuePairs.add(new BasicNameValuePair("email1", "email2"));
+            nameValuePairs.add(new BasicNameValuePair("email1", "slgjlskjgsg"));
+            nameValuePairs.add(new BasicNameValuePair("email2", "xkjfhgkdjfhgkdjfg"));
+
+            post.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+            HttpResponse response = client.execute(post);
+            final String response_str = response.toString();
+        }
+        catch (Exception ex){
+            Log.e("Debug", "error: " + ex.getMessage(), ex);
+        }
+    }
     public void openCamera(View view){
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         imageFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "flower.jpg");
-        Uri tmpuri = Uri.fromFile(imageFile);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, tmpuri);
+        imgUri = Uri.fromFile(imageFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imgUri);
         intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 0);
         startActivityForResult(intent, 0);
         flowerName.setText("rose");
@@ -63,22 +103,25 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        flowerName.setText("requestCode " + requestCode +" --- " + resultCode + " --- "+imageFile.exists());
-        switch (requestCode){
-            case Activity.RESULT_OK:
+        flowerName.setText("requestCode " + requestCode +" --- " + resultCode + " --- "+imageFile.getAbsolutePath());
+
                 if(imageFile.exists()){
                     Toast.makeText(this, imageFile.getAbsolutePath(), Toast.LENGTH_LONG).show();
+                    getContentResolver().notifyChange(imgUri, null);
+                    ImageView imageView =  (ImageView)findViewById(R.id.photo);
+                    ContentResolver cr = getContentResolver();
+                    Bitmap bitmap;
+                    try{
+                        bitmap = MediaStore.Images.Media.getBitmap(cr, imgUri);
+                        imageView.setImageBitmap(bitmap);
+                    }catch(Exception e){
+
+                    }
+
                 }else{
                     Toast.makeText(this, "error when saving", Toast.LENGTH_LONG).show();
                 }
-                break;
-            case Activity.RESULT_CANCELED:
-    //            Toast.makeText(this, imageFile.getAbsolutePath(), Toast.LENGTH_LONG).show();
-                Toast.makeText(this, "canceled", Toast.LENGTH_LONG).show();
-                break;
-            default:
-                break;
-        }
+
     }
 
     public void openFile(View view){
